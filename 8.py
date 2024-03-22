6a35eed841d3d5c5f6edd44632978909948c6191
akhramchikhina1 <a.khramchikhina1@g.nsu.ru>
2024-03-22 19:27:34 +0700

    print 8

--- 8.py
+++ 8.py
@@ -1,16 +1,15 @@
-
 knuts = int(input("Введите количество кнатов: "))
 
 galleons = knuts // (17 * 29)
 sikles = (knuts - galleons * 17 * 29) // 29
-knuts_remain = knuts - galleons * 17 * 29 - sikles * 29
+knuts_secound = knuts - galleons * 17 * 29 - sikles * 29
 
 result = ''
 if galleons > 0:
     result += str(galleons) + " галлеонов "
 if sikles > 0:
     result += str(sikles) + " сиклей "
-if knuts_remain > 0:
-    result += str(knuts_remain) + " кнатов"
+if knuts_secound > 0:
+    result += str(knuts_secound) + " кнатов"
 
 print(result)
