fe52cabc39fe2792a53ad351dc315e8b0c477a41
akhramchikhina1 <a.khramchikhina1@g.nsu.ru>
2024-03-22 09:23:32 +0700

    print 4

--- 4.py
+++ 4.py
@@ -1,5 +1,14 @@
-number=str(input('Введите четырехзначное число: '))
-if number[0]==number[-1] and number[1]==number[-2]:
-    print('настоящее')
+num = int(input("Введите число от 1 до 100: "))
+
+if num == 1:
+    print(num, "попугай")
+elif 2 <= num <= 4:
+    print(num, "попугая")
+elif 5 <= num <= 20:
+    print(num, "попугаев")
+elif num % 10 == 1:
+    print(num, "попугай")
+elif num % 10 == 2 or num % 10 == 3 or num % 10 == 4:
+    print(num, "попугая")
 else:
-    print('кривое')
+    print(num, "попугаев")
