2a3ef26ebcfafdd382dc21c3fb43288c4f7131d4
akhramchikhina1 <a.khramchikhina1@g.nsu.ru>
2024-03-22 19:01:04 +0700

    print 7

--- 7.py
+++ 7.py
@@ -1,8 +1,8 @@
 n, k, m = map(int, input().split())
 
 if k <= m:
-    stops = min(m - k, k + n - (m + 1))
+    stops = min(m - (k+1), k + n - (m + 1))
 else:
-    stops = min(k - m, m + n - (k + 1))
+    stops = min(k - (m+1), m + n - (k + 1))
 
 print(stops)
